67a7b138b076e179e714d107e4173132d1ceff2c
david catalan <catalan@adobe.com>
2024-09-18 09:36:49 +0200

    Add accepted blocks from AEM Block Assistant

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,18 +1,35 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
+  // Apply the 'cards' class to the block
+  block.classList.add('cards');
+
+  // Loop through each card
+  const cards = block.querySelectorAll('.cards > div');
+  cards.forEach((card) => {
+    // Add class to each card
+    card.classList.add('card');
+
+    // Add class to image container
+    const imageContainer = card.querySelector('picture').parentElement;
+    imageContainer.classList.add('card-image');
+
+    // Add class to content container
+    const contentContainer = imageContainer.nextElementSibling;
+    contentContainer.classList.add('card-content');
+
+    // Add class to description
+    const description = contentContainer.querySelector('p');
+    description.classList.add('card-description');
+
+    // Add class to title
+    const title = contentContainer.querySelector('h3');
+    title.classList.add('card-title');
+
+    // Add class to link container
+    const linkContainer = contentContainer.querySelector('p:last-of-type');
+    linkContainer.classList.add('card-link-container');
+
+    // Add class to link
+    const link = linkContainer.querySelector('a');
+    link.classList.add('card-link');
   });
-  ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
 }
